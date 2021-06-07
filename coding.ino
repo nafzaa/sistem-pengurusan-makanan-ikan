115bd1b2cd4a5c6380b29fd89bf38729bed6640a
nafzaa <78883615+nafzaa@users.noreply.github.com>
2021-06-07 23:27:01 +0800

    Update coding.ino

--- coding.ino
+++ coding.ino
@@ -3,8 +3,26 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <DS3232RTC.h>     
-DS3232RTC myRTC;
 #include <Servo.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+DS3232RTC myRTC;
+
+//for ds18b20
+#define ONE_WIRE_BUS D4 
+#define SensorPin A0         // the pH meter Analog output is connected with the Arduino’s Analog
+unsigned long int avgValue;  //Store the average value of the sensor feedback
+
+OneWire oneWire(ONE_WIRE_BUS);      
+DallasTemperature sensors(&oneWire); // pass oneWire to DallasTemperature library
+
+float b;
+float tempCelsius; 
+int buf[10],temp;
+
+int hourssenddata = 0;
+int minutessenddata = 0;
 
 String hours;
 String minutes;
@@ -17,6 +35,9 @@ String minit1;
 String timer = "";
 String timer1 = "";
 
+String phstring = "";
+String tempstring = "";
+
 const char* address;
 const char* timerjson;
 
@@ -24,6 +45,7 @@ Servo myservo;
 
 int state = 0;
 int state2 = 0;
+int state3 = 0;
 
 const char* wifiName = "Poco";
 const char* wifiPass = "nafza9494";
@@ -31,11 +53,14 @@ const char* wifiPass = "nafza9494";
 //Web Server address to read/write from 
 const char *host = "http://192.168.202.206/testapi/api.php?id=1";
 
+String host2;
+
 String data2;
 
 void setup() {
   
   Serial.begin(9600);
+  sensors.begin();
   myRTC.begin();
   myservo.attach(D6);
   myservo.write(0);
@@ -59,6 +84,67 @@ void setup() {
 }
 
 void loop() {
+  Readsensor(); 
+  getdata();  
+  delay(1000);
+  
+  display(); 
+  Serial.println(timer);
+
+  if (hour(t) == hourssenddata && minute(t) == minutessenddata){
+    hourssenddata++;
+    senddata();
+    }
+
+  if (timer == timerjson){     
+        if (state == 0){
+          state2 = 0;
+          Serial.println("servo on");
+          myservo.write(180);
+          delay(1000);
+          myservo.write(0);
+          delay(1000);
+          state = 1;  
+          }  
+      }
+
+  if (timer == timer1){
+    Serial.println("update state 0");
+    state = 0;
+    }
+  
+}
+
+void senddata(){
+  HTTPClient http2;    //Declare object of class HTTPClient
+
+  Serial.print("Request Link:");
+  Serial.println(host2);
+  
+  http2.begin(host2);     //Specify request destination
+  
+  int httpCode2 = http2.GET();            //Send the request
+  String payload2 = http2.getString();    //Get the response payload from server
+
+  Serial.print("Response Code:"); //200 is OK
+  Serial.println(httpCode2);   //Print HTTP return code
+
+  Serial.print("Returned data from Server:");
+  Serial.println(payload2);    //Print request response payload
+  
+  if(httpCode2 == 200)
+  {
+    Serial.println("send data oky");
+  }
+  else
+  {
+    Serial.println("Error in response");
+  }
+
+  http2.end();  //Close connection
+  }
+
+void getdata(){
   HTTPClient http;    //Declare object of class HTTPClient
 
   Serial.print("Request Link:");
@@ -69,7 +155,7 @@ void loop() {
   int httpCode = http.GET();            //Send the request
   String payload = http.getString();    //Get the response payload from server
 
-  data2 = payload.substring(1,53);
+  data2 = payload.substring(1,payload.length()-1);
 
   Serial.print("Response Code:"); //200 is OK
   Serial.println(httpCode);   //Print HTTP return code
@@ -106,30 +192,7 @@ void loop() {
   }
 
   http.end();  //Close connection
-  
-  delay(1000);  //GET Data at every 5 seconds
-  display();
-  Serial.println(timer);
-
-  if (timer == timerjson){     
-        if (state == 0){
-          state2 = 0;
-          Serial.println("servo on");
-          myservo.write(180);
-          delay(1000);
-          myservo.write(0);
-          delay(1000);
-          state = 1;  
-          }  
-      }
-
-  if (timer == timer1){
-    Serial.println("update state 0");
-    state = 0;
-    }
-
-  
-}
+  }
 
 
 void display()
@@ -138,6 +201,19 @@ void display()
     time_t t = myRTC.get();
 //    sprintf(buf, "%.2d:%.2d ",
 //        hour(t), minute(t));
+    
+    if (state3 == 0){
+      hourssenddata = hour(t);
+      minutessenddata = minute(t);
+      state3 = 1;
+      }
+
+    
+    if (hour(t) == 0){
+      hourssenddata =0 ;
+      }
+
+      
     hours = String(hour(t));
     minutes = String (minute(t));
 
@@ -172,3 +248,44 @@ void display()
     //Serial.println (timer);
     
 }
+
+void Readsensor(){
+  sensors.requestTemperatures();             // send the command to get temperatures
+  tempCelsius = sensors.getTempCByIndex(0);  // read temperature in Celsius
+
+  
+  for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(100);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+    avgValue+=buf[i];
+  float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  phValue=3.5*phValue; 
+
+  Serial.print("Temperature: ");
+  Serial.print(tempCelsius);    // print the temperature in Celsius
+  Serial.print("°C");
+  Serial.print(" ");
+  Serial.print("pH: ");  
+  Serial.println(phValue,2);
+
+  phstring = String(phValue,2);
+  tempstring = String(tempCelsius);
+
+  host2 = "http://192.168.202.206/testapi/insertdata.php?ph=" + phstring + "&temp=" + tempstring;
+  }
